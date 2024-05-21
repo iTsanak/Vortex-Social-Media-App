3357f84919d772ce15f79bc4c03c6f3b2879ac13
iTsanak <itsanachtsidis@myseneca.ca>
2024-05-20 23:27:47 -0400

    Adding RightSidebar.tsx for the "Top Creators" page

--- src/components/ui/shared/RightSidebar.tsx
+++ src/components/ui/shared/RightSidebar.tsx
@@ -1,31 +1,23 @@
 // src/components/ui/shared/RightSidebar.tsx
 
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useGetUsers } from '@/lib/react-query/queriesAndMutations'; //TO DO: import useGetUsers from queriesAndMutations
-import Loader from './Loader'; 
-import { IUser } from '@/types'; 
+import UserContent from './UserContent';
+import { useGetUsers } from '@/lib/react-query/queriesAndMutations';
+import Loader from './Loader';
 
 const RightSidebar = () => {
-  const { data: users, isLoading, isError } = useGetUsers();
-
-  if (isLoading) return <Loader />;
-  if (isError) return <div>Error loading users</div>;
+  const { data: users, isLoading: isUserLoading, isError: isErrorCreators } = useGetUsers();
 
   return (
-    <aside className='rightsidebar hidden md:block'>
+    <aside className='rightsidebar hidden md:block px-6 py-10 bg-dark-2 min-w-[300px]'>
       <h2 className='h3-bold md:h2-bold text-left w-full'>Top Creators</h2>
-      <div className='grid grid-cols-1 gap-4'>
-        {users?.map((user: IUser) => (
-          <Link to={`/profile/${user.id}`} key={user.id} className='flex items-center gap-4 p-4 hover:bg-gray-200'>
-            <img src={user.imageUrl || '/assets/icons/profile-placeholder.svg'} alt='profile' className='h-14 w-14 rounded-full' />
-            <div className='flex flex-col'>
-              <p className='body-bold'>{user.name}</p>
-              <p className='small-regular text-light-3'>@{user.username}</p>
-            </div>
-          </Link>
-        ))}
-      </div>
+      {isUserLoading ? (
+        <Loader />
+      ) : isErrorCreators ? (
+        <div>Error loading users</div>
+      ) : (
+        <UserContent users={users} />
+      )}
     </aside>
   );
 };
