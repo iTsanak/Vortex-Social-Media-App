01671ae22f5faf8379483b9f4a8e790002ec6260
iTsanak <itsanachtsidis@myseneca.ca>
2024-05-20 23:30:24 -0400

    API file updated and added the getUsers function to retrieve the users for "Top creators"

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -394,3 +394,19 @@ export async function searchPosts(searchTerm: string){
 //export async function getUsers() {}
 // I added the Rightsidebar file and and put in the root layout need to do the function
 //above and import in the rightsidebar file
+// Get the users from the database
+export async function getUsers(limit?: number) {
+    try {
+        const users = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            [Query.orderDesc("$createdAt"), Query.limit(limit? limit : 8)], //double check this limit maybe leave as 10 only
+        )
+
+        if(!users) throw Error;
+        return users.documents;
+    } catch(error){
+        console.log(error);
+        return [];
+    }
+}
